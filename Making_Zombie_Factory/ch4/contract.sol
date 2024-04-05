2ccde15811b54a03e2ab1703a15012453c3b99d2
Himanish kochhar <himanish.kochhar@gmail.com>
2024-04-05 10:57:32 +0530

    ch5 structs introduced

--- Making_Zombie_Factory/ch4/contract.sol
+++ Making_Zombie_Factory/ch4/contract.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.5.0 <0.6.0;
 contract ZombieFactory {
 
     uint dnaDigits = 16;
-    uint dnaModulus = 10 ^ dnaDigits
+    uint dnaModulus = 10 ** dnaDigits; // this means x ^ y that is 10 raise to power dnaDigits
 }
